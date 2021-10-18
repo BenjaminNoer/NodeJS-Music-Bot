51c7422f2654ea4dd0aedffb8379194011329b4f
Benjamin Noer <60319493+BenjaminNoer@users.noreply.github.com>
2021-10-19 00:47:42 +0100

    Update main.js
    Added comments

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES"] })
 
+//Messages starting with the prefix are seen as commands by the bot
 const prefix = '-';
 
 const fs = require('fs');
 
 client.commands = new Discord.Collection();
 
+//Find all commands as seperate .js files
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for (const file of commandFiles)
 {
@@ -20,7 +22,9 @@ client.once('ready', () => {
     console.log('Bot ready');
 });
 
+//Triggered when a message is sent to a channel the bot can see
 client.on('message', message => {
+    //Ignore message if it does not start with the prefix or was sent by a bot
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
